5a1c85542f2825c4321e8e87e0901b2df8577fb8
Aaron Saban <aaronsaban98@gmail.com>
2020-09-10 23:30:45 -0300

    Loom: Ejercicio uno finalizado - Conjuntos

---
+++ loom/practica1/conjunto.py
@@ -0,0 +1,49 @@
+class Conjunto:
+
+    def __init__(self, valores):
+        self.__conjunto = self.__eliminar_duplicados(valores)
+
+    def obtener_valor(self):
+        return self.__conjunto
+
+    def __eliminar_duplicados(self, lista):
+        lista_sin_duplicados = []
+        for elemento in lista:
+            if elemento not in lista_sin_duplicados:
+                lista_sin_duplicados.append(elemento)
+        return lista_sin_duplicados
+
+    def __union(self, conjuntoA, conjuntoB):
+        return self.__eliminar_duplicados(conjuntoA + conjuntoB)
+
+    def __interseccion(self, conjuntoA, conjuntoB):
+        conjunto_interseccion = []
+        for elemento in conjuntoB:
+            if elemento in conjuntoA and elemento not in conjunto_interseccion:
+                conjunto_interseccion.append(elemento)
+            
+        return conjunto_interseccion        
+    
+    def union(self, conjuntoB):
+        return self.__union(self.__conjunto, conjuntoB.obtener_valor())
+
+    def interseccion(self, conjuntoB):        
+        return self.__interseccion(self.__conjunto, conjuntoB.obtener_valor())
+
+    def diferencia(self, conjuntoB):
+        return [elemento for elemento in self.__conjunto if elemento not in conjuntoB.obtener_valor()]
+    
+    def diferencia_asimetrica(self, conjuntoB):    
+        return self.__union(self.diferencia(b), conjuntoB.diferencia(a)) 
+        
+    def __repr__(self):
+        return self.__conjunto
+    
+
+a = Conjunto([1,2])
+b = Conjunto([2,3])
+
+print(a.union(b))
+print(a.interseccion(b))
+print(a.diferencia(b))
+print(a.diferencia_asimetrica(b))
