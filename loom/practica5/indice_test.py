34c783044ede3053ac33355e7e38f962f7d2b43c
Aaron Saban <aaronsaban98@gmail.com>
2020-10-19 11:40:16 -0300

    loom: ejercicio 5 -  agregado tests

--- loom/practica5/indice_test.py
+++ loom/practica5/indice_test.py
@@ -1,8 +1,47 @@
 import unittest
 from main import IndiceNovelas
+from excepciones import NoSeEncontroPalabra
 
 class TestIndiceInvertido(unittest.TestCase):
 
     def __init__(self, *args, **kwars):
         super().__init__(*args, **kwars)
+        self._indice = IndiceNovelas("test.txt", novelas=[
+            "LA BUENAVENTURA",
+            "LA CORNETA DE LLAVES",
+            "LAS DOS GLORIAS",
+            "EL AFRANCESADO",
+            "¡VIVA EL PAPA!",
+            "EL EXTRANJERO",
+            "EL LIBRO TALONARIO",
+            "MOROS Y CRISTIANOS",
+            "EL AÑO EN SPITZBERG"
+        ])
+
+
+    def _testear_busqueda_de_palabra(self, resultado_esperado, palabra):
+        resultado = self._indice.buscar(palabra)
+        self.assertEquals(resultado_esperado, resultado)
+    
+    def test_buscar_palabra_agosto(self):
+        resultado_esperado = {'agosto'}, {'LA BUENAVENTURA', 'EL AÑO EN SPITZBERG'}
+        self._testear_busqueda_de_palabra(resultado_esperado, "Agosto")
+
+    def test_buscar_palabra_con_numeros(self):
+        resultado_esperado = {'1816'}, {'LA BUENAVENTURA'}
+        self._testear_busqueda_de_palabra(resultado_esperado, "1816")
+
+    def test_buscar_palabra_con_comodin(self):
+        resultado_esperado = {'abrazarlo', 'abrazo'}, {'LAS DOS GLORIAS', 'LA BUENAVENTURA'}
+        self._testear_busqueda_de_palabra(resultado_esperado, "abra*")
+    
+    def test_buscar_palabra_que_no_esta_en_el_indice(self):
+        with self.assertRaises(NoSeEncontroPalabra): self._indice.buscar("palabraquenoestaenelindiceporqueesmuyfacha")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
 
